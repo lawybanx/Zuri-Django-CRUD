ff4d13ba048d486bf62b6e0bc3e63cf493e3d907
Awwal Bankole <awwalbankole@gmail.com>
2022-06-21 20:23:10 +0100

    Completed task

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,34 @@
+from msilib.schema import ListView, CreateView, DetailView, UpdateView, DeleteView
 from django.shortcuts import render
+from django.urls import reverse_lazy
+
+from blog.models import Post
 
 # Create your views here.
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/home.html"
+    context_object_name = "all_posts_list"
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = '__all__'
+    success_url = reverse_lazy('blog:all')
+
+
+class PostDetailView(DetailView):
+    model = Post 
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = '__all__'
+    success_url = reverse_lazy('blog:all')
+    
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog:all')
