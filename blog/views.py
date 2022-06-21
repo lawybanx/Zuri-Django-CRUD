edb54f9131aa92b12f73f9133dbd0e855f527b59
Awwal Bankole <awwalbankole@gmail.com>
2022-06-21 20:51:52 +0100

    Completed task

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from msilib.schema import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.shortcuts import render
 from django.urls import reverse_lazy
 
@@ -9,8 +9,6 @@ from blog.models import Post
 
 class PostListView(ListView):
     model = Post
-    template_name = "blog/home.html"
-    context_object_name = "all_posts_list"
 
 
 class PostCreateView(CreateView):
@@ -20,14 +18,14 @@ class PostCreateView(CreateView):
 
 
 class PostDetailView(DetailView):
-    model = Post 
+    model = Post
 
 
 class PostUpdateView(UpdateView):
     model = Post
     fields = '__all__'
     success_url = reverse_lazy('blog:all')
-    
+
 
 class PostDeleteView(DeleteView):
     model = Post
